2540c7d732ece69db0e5aa6027fc5fb43eb73da3
Davi Ribeiro <daviribeiro@MacBook-Air-de-Davi.local>
2023-07-16 20:50:09 -0300

    Correções no JavaScript alteradas

--- scripts/main.js
+++ scripts/main.js
@@ -13,7 +13,7 @@ $(document).ready(function(){
         $(` 
         <div class="card">
             <div class="card-body">
-                <h1 class="card-title">${tituloNoticia}</h1>
+                <h3 class="card-title">${tituloNoticia}</h3>
                 <p class="card-text">${textoNoticia}</p>
             </div>
                 <div class="card-footer text-center">
@@ -37,38 +37,8 @@ $(document).ready(function(){
         novaCategoria = $(`<option value="${nomeCategoria}">${nomeCategoria}</option>`);
         $(novaCategoria).appendTo('.form-cat');
         $('#nomeCategoria').val(' ');
-        alert('Notícia Cadastrada com sucesso!');
+        alert('Categoria Cadastrada com sucesso!');
 
-    });
-
-    $('.form-noticia').on('submit', function(e) {
-        e.preventDefault();
-        
-        const tituloNoticia = $('#tituloNoticia').val();
-        const descricaoNoticia = $('#descricaoNoticia').val();
-        const textoNoticia = $('#textoNoticia').val();
-        console.log('Botao cadastrar clicado');
-
-        novaNoticia = $('<div class="col-sm-12 col-md-6 col-lg-4 p-4"></div');
-
-        $(` 
-        <div class="card">
-            <div class="card-body">
-                <h1 class="card-title">${tituloNoticia}</h1>
-                <p class="card-text">${textoNoticia}</p>
-            </div>
-                <div class="card-footer text-center">
-                    <a href="" class="btn btn-primary">SAIBA MAIS</a>
-                </div>
-        </div>
-        `).appendTo(novaNoticia);
-
-        $(novaNoticia).appendTo('.noticias');
-
-        $('#tituloNoticia').val(' ');
-        $('#textoNoticia').val(' ');
-        alert('Notícia Cadastrada com sucesso!');
-        
     });
 
 })
